278cc9cedcf6979fa476e0dbdf5790b648f4fff7
Tyler Christian <tylerc71197@gmail.com>
2020-02-03 14:44:32 -0500

    Added view Controll files

---
+++ WaterMyPlants/Keys/Keys.swift
@@ -0,0 +1,20 @@
+//
+//  Keys.swift
+//  WaterMyPlants
+//
+//  Created by Lambda_School_Loaner_218 on 2/3/20.
+//  Copyright © 2020 WaterMyPlants3. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    
+    static let signUpSegue = "SignupSegue"
+    static let logInSegue = "LoginSegue"
+    static let signToTableView = "SigntoTableView"
+    static let logInToTableView = "loginTableView"
+    static let PlantDetialSegue = "PlantDetialSegue"
+    static let AddPlantSegue = "AddPlantSegue"
+    
+}
