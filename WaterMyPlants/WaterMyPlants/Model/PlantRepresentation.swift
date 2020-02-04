abb59fc03243483998a684b9657fe7b2f36ad0e6
Patrick Millet <lohs79seahawk@gmail.com>
2020-02-04 16:43:43 -0600

    Adding notifications and updated representations

--- WaterMyPlants/WaterMyPlants/Model/PlantRepresentation.swift
+++ WaterMyPlants/WaterMyPlants/Model/PlantRepresentation.swift
@@ -15,21 +15,14 @@ struct PlantRepresentation: Equatable, Codable {
     var nickname: String
     var imageName: String
     var identifier: UUID?
-    var plantImage: UIImage {
+    var species: String?
+    var h2oFrequency: Int64
+    
+    var plantImage: UIImage? {
          UIImage(named: imageName)!
     }
-    var h2oFrequency: Int64
-    var h2oDate: Date {
-        let dateformatter = DateFormatter()
-        let newDate = Date().timeIntervalSinceNow
-        let dividend = h2oFrequency
-        let intDate = Int(newDate)
-        return Date()
-        
-        // Need to find a way to take Int from server and convert int into Date object and display notifications at intervals matching the returned Int
-    }
-    var species: String
 }
+
 
 struct PlantRepresentations: Codable {
     let results: [PlantRepresentation]
